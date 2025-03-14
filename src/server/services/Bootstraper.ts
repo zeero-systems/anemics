4522c261bc17d49fc77cbf6130e0a474f71bc9e3
Eduardo Segura <eduardo@zxxxro.com>
2025-03-13 22:09:37 -0300

    fix: added application return type

--- src/server/services/Bootstraper.ts
+++ src/server/services/Bootstraper.ts
@@ -2,7 +2,7 @@ import { ConstructorType, Factory } from '@zxxxro/commons';
 import { Application } from '~/server/services/Application.ts';
 
 export class Bootstraper {
-  public static create(target: ConstructorType<any>) {
+  public static create(target: ConstructorType<any>): Application {
     return new Application(Factory.construct(target))
   }
 }
