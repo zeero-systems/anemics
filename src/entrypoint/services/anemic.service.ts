8328c7e471ffbe6c86fc606cd475474062446d0c
Eduardo Segura <eduardo@zxxxro.com>
2025-10-18 11:29:48 -0300

    fix: eventType type duplication

--- src/entrypoint/services/anemic.service.ts
+++ src/entrypoint/services/anemic.service.ts
@@ -1,5 +1,4 @@
-import type { PackInterface} from '@zeero/commons';
-import type { EventType } from '~/entrypoint/types.ts';
+import type { PackInterface } from '@zeero/commons';
 import type { ServerOptionsType } from '~/network/types.ts';
 import type { AnemicInterface, ApplicationInterface } from '~/entrypoint/interfaces.ts';
 import type { MiddlewareInterface } from '~/controller/interfaces.ts';
@@ -13,73 +12,74 @@ import MethodEnum from '~/network/enums/method.enum.ts';
 import EventEnum from '~/controller/enums/event.enum.ts';
 
 export class Anemic implements AnemicInterface {
-  private dispatcher = new Dispatcher<EventType>()
-  
+  private dispatcher = new Dispatcher<{
+    boot: [ApplicationInterface];
+    start: [ApplicationInterface];
+    stop: [ApplicationInterface];
+  }>();
+
   constructor(public application: ApplicationInterface) {
-    for(const server of this.application.servers) {
+    for (const server of this.application.servers) {
       if (server.accepts.includes(MethodEnum.SOCKET)) {
-        this.dispatcher.subscribe('start', () => server.start((socket) => this.socketHandler(socket, server.options)))
+        this.dispatcher.subscribe('start', () => server.start((socket) => this.socketHandler(socket, server.options)));
       } else {
-        this.dispatcher.subscribe('start', () => server.start((request) => this.httpHandler(request, server.options)))
+        this.dispatcher.subscribe('start', () => server.start((request) => this.httpHandler(request, server.options)));
       }
-      this.dispatcher.subscribe('stop', () => server.stop())
+      this.dispatcher.subscribe('stop', () => server.stop());
     }
 
-    for(const packName of application.packer.packs) {
-      const pack = application.packer.container.construct<PackInterface>(packName)
-      
-      if (pack?.onBoot) this.dispatcher.subscribe('boot', pack?.onBoot)
-      if (pack?.onStart) this.dispatcher.subscribe('start', pack?.onStart)
-      if (pack?.onStop) this.dispatcher.subscribe('stop', pack?.onStop)
+    for (const packName of application.packer.packs) {
+      const pack = application.packer.container.construct<PackInterface>(packName);
+
+      if (pack?.onBoot) this.dispatcher.subscribe('boot', pack?.onBoot);
+      if (pack?.onStart) this.dispatcher.subscribe('start', pack?.onStart);
+      if (pack?.onStop) this.dispatcher.subscribe('stop', pack?.onStop);
     }
   }
 
   private async httpHandler(request: RequestInterface, server: ServerOptionsType): Promise<Response> {
-    const route = this.application.router.find(request.url, request.method.toLowerCase() as any)
+    const route = this.application.router.find(request.url, request.method.toLowerCase() as any);
 
-    if (!route) return new Response(null, { status: 404 })
-    
-    const url = route.pattern?.exec(request.url) as URLPatternResult
-    const key = `${String(route.controller.key)}:${route.action.key}`
-    const container = this.application.packer.container.duplicate()
-    const response = new Responser()
-    
+    if (!route) return new Response(null, { status: 404 });
+
+    const url = route.pattern?.exec(request.url) as URLPatternResult;
+    const key = `${String(route.controller.key)}:${route.action.key}`;
+    const container = this.application.packer.container.duplicate();
+    const response = new Responser();
+
     container.add([
       { name: 'Request', target: request },
       { name: 'Response', target: response },
       { name: 'Server', target: server },
       { name: 'Url', target: url },
-    ], 'provider')
+    ], 'provider');
 
-    const context = { request, response, container, route, server, url }
-    
+    const context = { request, response, container, route, server, url };
+
     try {
-      
-      let next: NextFunctionType = async () => {}
+      let next: NextFunctionType = async () => {};
 
       if (this.application.middler.middlewares[key][EventEnum.AFTER]) {
-        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.AFTER], next)
+        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.AFTER], next);
       }
-  
+
       if (this.application.middler.middlewares[key][EventEnum.MIDDLE]) {
-        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.MIDDLE], next)
+        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.MIDDLE], next);
       }
-  
+
       if (this.application.middler.middlewares[key][EventEnum.BEFORE]) {
-        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.BEFORE], next)
-      }    
-        
-      await next()
+        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.BEFORE], next);
+      }
 
+      await next();
     } catch {
-
-      let next: NextFunctionType = async () => {}
+      let next: NextFunctionType = async () => {};
 
       if (this.application.middler.middlewares[key][EventEnum.EXCEPTION]) {
-        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.EXCEPTION], next)
+        next = this.nextMiddleware(context, this.application.middler.middlewares[key][EventEnum.EXCEPTION], next);
       }
 
-      await next()
+      await next();
     }
 
     return new Response(response.body, {
@@ -90,27 +90,31 @@ export class Anemic implements AnemicInterface {
   }
 
   private async socketHandler(socket: WebSocket, server: ServerOptionsType): Promise<Response> {
-    throw new Error('Not implemented')    
+    throw new Error('Not implemented');
   }
-  
-  private nextMiddleware(context: ContextType, middlewares: MiddlewareInterface[], lastNext: NextFunctionType): NextFunctionType {
+
+  private nextMiddleware(
+    context: ContextType,
+    middlewares: MiddlewareInterface[],
+    lastNext: NextFunctionType,
+  ): NextFunctionType {
     return middlewares.reduce((a: NextFunctionType, b: MiddlewareInterface) => {
       return (async (): Promise<void> => {
-        return await b.onUse(context, a)
-      }) as NextFunctionType
-    }, lastNext)
+        return await b.onUse(context, a);
+      }) as NextFunctionType;
+    }, lastNext);
   }
-  
+
   public async boot(): Promise<void> {
-    return this.dispatcher.dispatch('boot', this.application)
+    return this.dispatcher.dispatch('boot', this.application);
   }
 
   public async start(): Promise<void> {
-    return this.dispatcher.dispatch('start', this.application)
+    return this.dispatcher.dispatch('start', this.application);
   }
 
   public async stop(): Promise<void> {
-    return this.dispatcher.dispatch('stop', this.application)
+    return this.dispatcher.dispatch('stop', this.application);
   }
 }
 
