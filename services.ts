b214711625795fdb0eb7cce00d7a0eba6b103c50
Eduardo Segura <eduardo@zxxxro.com>
2025-10-26 18:10:37 -0300

    fix: export databases implementations

--- services.ts
+++ services.ts
@@ -12,6 +12,8 @@ import Filter from '~/persister/services/filter.service.ts';
 import Query from '~/persister/services/query.service.ts';
 import Repository from '~/persister/services/repository.ts';
 import Table from '~/persister/services/table.service.ts';
+import Memory from '~/persister/memory/memory.database.ts';
+import Postgresql from '~/persister/postgresql/postgresql.database.ts';
 
 import Builder from '~/querier/services/builder.services.ts';
 import Querier from '~/querier/services/querier.service.ts';
@@ -30,22 +32,26 @@ export { default as Filter } from '~/persister/services/filter.service.ts';
 export { default as Query } from '~/persister/services/query.service.ts';
 export { default as Repository } from '~/persister/services/repository.ts';
 export { default as Table } from '~/persister/services/table.service.ts';
+export { default as Memory } from '~/persister/memory/memory.database.ts';
+export { default as Postgresql } from '~/persister/postgresql/postgresql.database.ts';
 
 export { default as Builder } from '~/querier/services/builder.services.ts';
 export { default as Querier } from '~/querier/services/querier.service.ts';
 
 export default {
-  Middler,
-  Router,
   Anemic,
   Application,
-  Ws,
-  Http,
-  Response,
+  Builder,
   Filter,
+  Http,
+  Memory,
+  Middler,
+  Postgresql,
+  Querier,
   Query,
   Repository,
+  Response,
+  Router,
   Table,
-  Builder,
-  Querier,
+  Ws,
 }
